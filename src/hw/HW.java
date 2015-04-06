89860ec408adc95f1fbb2038a63541e0f6861201
ainunfauziyahbahary <ainunfauziyahbahary@gmail.com>
2015-04-06 22:49:44 +0700

    new commit

---
+++ src/hw/HW.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package hw;
+
+/**
+ *
+ * @author Ainun
+ */
+public class HW {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello, Nama saya Ainun Fauziyah Bahary dengan NIM 6306134099");
+        System.out.println("Saya sangat menyukai praktikum RPL modul 8 ini");
+    }
+    
+}
